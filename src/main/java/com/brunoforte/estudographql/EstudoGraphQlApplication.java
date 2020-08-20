f62b4eea5cea08dd738fb9f1d82b65d6333f85f5
Bruno Forte <bruno@fforte.biz>
2020-08-20 18:01:16 -0300

    Starter build

---
+++ src/main/java/com/brunoforte/estudographql/EstudoGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.brunoforte.estudographql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunoforte/estudographql/EstudoGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoforte.estudographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
